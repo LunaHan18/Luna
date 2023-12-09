0945a1e7e111cfd40edd6641316fa1b2054b5350
LunaHan18 <jh4756@columbia.edu>
2023-12-10 02:59:32 +0800

    1.index

---
+++ exercises/Week 09/starter_code_week_09/arrays_and_loops/main.js
@@ -0,0 +1,14 @@
+/*
+    We have an array of people's ages, saved in the variable ages. 
+    Write a for-loop that will print each of the elements to the console.
+    let ages = [29, 27, 24, 31, 23, 28, 26, 30, 25, 23]
+
+    We have an array of names, saved in the variable names.
+    Write a for-loop that will print each of the elements to the console.
+    let names = ["Olivia", "Ethan", "Isabella", "Liam", "Sophia", "Noah", "Ava", "Mason"]
+*/
+
+let ages = [29, 27, 24, 31, 23, 28, 26, 30, 25, 23];
+for(let i=1; i<=ages.length; i++){
+    console.log(ages[i]);
+}
